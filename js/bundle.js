7468c8686cccb88d687761a783673eb358589205
Francesco Orlando <franzesqo@gmail.com>
2026-01-28 17:26:32 +0100

    managed by update.sh Wed Jan 28 05:26:32 PM CET 2026

--- js/bundle.js
+++ js/bundle.js
@@ -2,6 +2,23 @@
   // <stdin>
   (function() {
     "use strict";
+    const getCachedTheme = () => {
+      const cachedTheme = localStorage.getItem("color-scheme");
+      return cachedTheme || "dark";
+    };
+    document.documentElement.setAttribute("color-scheme", getCachedTheme());
+    window.addEventListener("DOMContentLoaded", () => {
+      requestAnimationFrame(() => document.body.classList.remove("notransition"));
+      document.getElementById("theme-switcher").addEventListener("click", (e) => {
+        e.preventDefault();
+        const state = getCachedTheme() === "light" ? "dark" : "light";
+        document.documentElement.setAttribute("color-scheme", state);
+        localStorage.setItem("color-scheme", state);
+      });
+    });
+  })();
+  (function() {
+    "use strict";
     window.addEventListener("DOMContentLoaded", () => {
       let menu = document.getElementById("nav-dropdown-menu");
       const menu_btn = document.getElementById("nav-dropdown-button");
@@ -12,24 +29,6 @@
         });
         window.addEventListener("scroll", () => {
         });
-      });
-    });
-  })();
-  (function() {
-    "use strict";
-    const getCachedTheme = () => {
-      const cachedTheme = localStorage.getItem("color-scheme");
-      const preferDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-      return cachedTheme || (preferDark ? "dark" : "light");
-    };
-    document.documentElement.setAttribute("color-scheme", getCachedTheme());
-    window.addEventListener("DOMContentLoaded", () => {
-      requestAnimationFrame(() => document.body.classList.remove("notransition"));
-      document.getElementById("theme-switcher").addEventListener("click", (e) => {
-        e.preventDefault();
-        const state = getCachedTheme() === "light" ? "dark" : "light";
-        document.documentElement.setAttribute("color-scheme", state);
-        localStorage.setItem("color-scheme", state);
       });
     });
   })();
